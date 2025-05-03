9319baa1f88b6dee54c5d6e0ed57302024710252
socratesioa <156105096+socratesioa@users.noreply.github.com>
2025-05-03 11:34:06 +0300

    Add files via upload

---
+++ main.js
@@ -0,0 +1,47 @@
+const form = document.getElementById("form");
+const emailInput = document.getElementById("email");
+const emailError = document.getElementById("email-error");
+
+const handleSubmit = (e) => {
+  e.preventDefault();
+
+  const emailError = document.getElementById("email-error");
+
+  emailError.classList.remove("input-error");
+  emailError.textContent = "";
+
+  const data = Object.fromEntries(new FormData(e.target));
+  const errors = {};
+
+  const email = data.email.trim();
+
+  if (!email) {
+    errors.email = "Please provide a valid email address";
+    emailInput.classList.add("input-error");
+  } else if (!/^[^\s@]+@[^\s@]+\.[^\s@]{2,}$/.test(email)) {
+    errors.email = "Please provide a valid email address";
+    emailInput.classList.add("input-error");
+  }
+
+  if (Object.keys(errors).length > 0) {
+    displayErrors(errors);
+  } else {
+    console.log("Form is Valid!", data);
+  }
+
+  function displayErrors(errors) {
+    for (const key in errors) {
+      const errorElement = document.getElementById(`${key}-error`);
+      if (errorElement) {
+        errorElement.textContent = errors[key];
+      }
+    }
+  }
+};
+
+emailInput.addEventListener("input", () => {
+  emailInput.classList.remove("input-error");
+  emailError.textContent = "";
+});
+
+form.addEventListener("submit", handleSubmit);
